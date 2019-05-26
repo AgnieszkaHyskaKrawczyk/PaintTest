9b6c45984450cfdee2411b4e6a79e301dcaa19ba
Agnieszka <agnieszka.krawczyk@seznam.cz>
2019-05-26 18:34:46 +0200

    all10

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics drawingSpace;
+            drawingSpace = e.Graphics;
+
+            drawingSpace.DrawLine(Pens.Black, 0, 0, 200, 100);
+            drawingSpace.DrawLine(Pens.Black, 0, 100, 200, 100);
+            drawingSpace.DrawRectangle(Pens.Blue, 200, 100, 50, 100);
+            drawingSpace.DrawEllipse(Pens.CadetBlue, 200, 100, 50, 100);
+            string s = "space";
+
+            PrintControllerWithStatusDialog();
+        }
+    }
+}
